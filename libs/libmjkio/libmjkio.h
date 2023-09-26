3f3a3f5c58f4eeb0e0e0ff964c24d23100196160
minjae Kim <hjn966@naver.com>
2023-09-26 14:54:30 +0900

    Change header

--- libs/libmjkio/libmjkio.h
+++ libs/libmjkio/libmjkio.h
@@ -3,18 +3,6 @@
 
 #include <stdbool.h>
 
-int convert_alpha_case(int c);
-int isupper(int c);
-void date();
-void dmesg();
-bool print_history();
-bool add_history(char *cmd);
-bool clear_history();
-char *get_history(int index);
-void ls();
 bool putstr_fd(int fd, const char *s);
-void tolower_str(char *str, int len);
-int tolower(int c);
-void uptime();
 
 #endif
